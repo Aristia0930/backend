46077dad813ecc2b813291eba0ed3fa9ea0e5698
kim <kjk9858@gmail.com>
2024-07-09 14:13:30 +0900

    ai 날씨 계절 별 내용 추가

--- src/main/java/org/example/backend/gemini/GeminiController.java
+++ src/main/java/org/example/backend/gemini/GeminiController.java
@@ -25,15 +25,16 @@ public class GeminiController {
         String menuList=geminiService.menuList();
         System.out.println(menuList);
         System.out.println(orderList);
+        String weather=geminiService.weather();
 
 
         try {
             if (orderList==""){
                 System.out.println("주문없음");
-                return ResponseEntity.ok().body(geminiService.getContents("음식 메뉴를 추천받고 싶어 주문할수 있는 음식들은 목록은" + menuList + "주문 가능한 음식 중 추천할 메뉴 3개만 추천해줘야해 다른 음식을 추천해주면 안되는거야 그리고 이때 대답결과는 1.메뉴이름:메뉴추천이유 이런 형식으로 나타내줘 그리고 단어 사이에 * 이 모양은 사용하지 말아죠 그리고 무조건 내가 가지고 있는 메뉴 목록안에서만 추천해줘야해", x, y));
+                return ResponseEntity.ok().body(geminiService.getContents("오늘의 "+weather+" 음식 메뉴를 추천받고 싶어 주문할수 있는 음식들은 목록은" + menuList + "주문 가능한 음식 중 추천할 메뉴 3개만 추천해줘야해 다른 음식을 추천해주면 안되는거야 그리고 이때 대답결과는 1.메뉴이름:메뉴추천이유 이런 형식으로 나타내줘 그리고 단어 사이에 * 이 모양은 사용하지 말아죠 그리고 무조건 내가 가지고 있는 메뉴 목록안에서만 추천해줘야해", x, y));
             }
             else {
-                return ResponseEntity.ok().body(geminiService.getContents("음식 메뉴를 추천받고 싶어 현재까지 내가 먹은 음식들은" + orderList + "주문할수 있는 음식들은 목록은" + menuList + "주문 가능한 음식 중 추천할 메뉴 3개만 추천해줘야해 다른 음식을 추천해주면 안되는거야 그리고 이때 대답결과는 1.메뉴이름:메뉴추천이유 이런 형식으로 나타내줘 그리고 단어 사이에 * 이 모양은 사용하지 말아죠 그리고 무조건 내가 가지고 있는 메뉴 목록안에서만 추천해줘야해", x, y));
+                return ResponseEntity.ok().body(geminiService.getContents("오늘의 "+weather+" 이고 현재까지 내가 먹은 음식들은" + orderList + "주문할수 있는 음식들은 목록은" + menuList + "주문 가능한 음식 중 날씨와 기온에 맞게 추천할 메뉴 3개만 추천해줘야해 다른 음식을 추천해주면 안되는거야 그리고 이때 대답결과는 1.메뉴이름:메뉴추천이유 이런 형식으로 나타내줘 그리고 단어 사이에 * 이 모양은 사용하지 말아죠 그리고 무조건 내가 가지고 있는 메뉴 목록안에서만 추천해줘야해", x, y));
             }
             }
         catch (HttpClientErrorException e) {
@@ -42,12 +43,12 @@ public class GeminiController {
     }
 
     @GetMapping("/chat2")
-    public ResponseEntity<?> gemini2() throws JsonProcessingException, JsonProcessingException {
+    public ResponseEntity<?> weather() throws JsonProcessingException {
 
 
         try {
 
-            return ResponseEntity.ok().body(geminiService.run());
+            return ResponseEntity.ok().body(geminiService.weather());
 
 
         }
